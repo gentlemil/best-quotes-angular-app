f48c2384bc72655899ca60e175a127ff3c6de9e1
milosz bukala <milosz.bukala@princity.com>
2020-08-22 15:07:19 +0200

    Preparation dependencies in the program.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { Component } from '@angular/core';
+import { QUOTES } from './models/database';
+import { Quotation } from './models/quotation';
 
-import { AppComponent } from './app.component';
-
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
-export class AppModule { }
+export class AppComponent {
+  // Pole do ukrywania i pokazywania formularza
+  showForm = false;
+  // nowe pole reprezentującę listę cytatów
+  quotes: Quotation[] = QUOTES;
+  // pole quotation reprezentuje pojedynczy cytat
+  quotation: Quotation = { author: '', sentence: '', votes: 0 };
+
+  // metoda obsługuje głosowanie na konkretny cytat
+  addVote(quotation: Quotation, value: number) {
+    quotation.votes += value;
+  }
+
+  // przełącza pole klasy true / false
+  onSwitchForm(): void {
+    this.showForm = !this.showForm;
+  }
+
+  // dodaje cytat na początek listy i resetuje pole quotation
+  addQuotation() {
+    this.quotes.unshift(this.quotation);
+    this.quotation = { author: '', sentence: '', votes: 0 };
+  }
+}
