3f703f39a4b23991f068e330f0a3a34fb250cf2e
milosz bukala <milosz.bukala@princity.com>
2020-08-23 13:40:24 +0200

    Form implementation, adding new quotes.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,10 +10,25 @@ import { Quotation } from './models/quotation';
 export class AppComponent {
   
   showForm = false;   // pole do wysw. formularza
-  quotes: Quotation[] = QUOTES;
+  quotes: Quotation[] = QUOTES;   //nowe pole repr.liste cytatow
+  quotation: Quotation = {
+    author: '',
+    sentence: '',
+    votes: 0
+  };  // pole w komp. do przechow. danych wpisywanych na stronie
 
   onSwitchForm(): void {
     this.showForm = !this.showForm;
   }
+
+  addQuotation() {
+    this.quotes.unshift(this.quotation);  //dodaje elem. na poczatek listy
+    this.quotation = {
+      author: '',
+      sentence: '',
+      votes: 0
+    };   // resetujemy obiekt quotation przypisujac mu puste wartosci
+  }
+
 
 }
